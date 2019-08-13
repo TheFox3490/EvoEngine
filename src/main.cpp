647fa716b7d3422e15f60c0760d84c79dd0e50d8
fox3490 <shilnikovnikita63@gmail.com>
2019-08-13 16:38:09 +0400

    nado

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,24 @@
 #include <stdio.h> 
 #include "Rstring.h" 
-#define obj_size 16 
+#define obj_size 15
 
 using namespace rt;
 
-<<<<<<< Updated upstream
-void draw_scene(Rstring *str) {
-=======
-void draw_scene(Rstring* str) {
->>>>>>> Stashed changes
+void draw_scene(Rstring* str, char border_char) {
+	for (uint64 i = 0; i <= obj_size + 1; i++)
+		printf("%s", (const char*)(Rstring(border_char) + ' '));
+	printf("%c", '\n');
 	for (uint64 i = 0; i < obj_size; i++)
-		printf("%s\n", (const char*)str[i]);
+		printf("%s\n", (const char*)(Rstring(". ") + str[i] + "."));
+	for (uint64 i = 0; i <= obj_size + 1; i++)
+		printf("%s", ". ");
 }
 
 int main(void) {
 	Rstring str[obj_size];
 	for (uint8 i = 0; i < obj_size; i++)
 		for (uint8 j = 0; j < obj_size; j++)
-			str[i] += "#";
-	draw_scene(str);
+			str[i] += "# ";
+	draw_scene(str, '.');
 	return 0;
 }
